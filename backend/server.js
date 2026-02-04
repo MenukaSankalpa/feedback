aae78b28f505f559d5d55d2226566832238030eb
H.Menuka Sankalpa <menuka.sankalpa@gmail.com>
2026-02-04 17:17:48 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,13 @@
 import express from "express";
 import cors from "cors";
-import authRoutes from "./routes/auth.js";
 import feedbackRoutes from "./routes/feedback.js";
+import adminRoutes from "./routes/admin.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/feedback", feedbackRoutes);
+app.use("/api/feedback", feedbackRoutes); // user feedback
+app.use("/api/admin", adminRoutes);       // admin/user login + fetch feedback
 
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
-});
+app.listen(5000, () => console.log("Server running on http://localhost:5000"));
