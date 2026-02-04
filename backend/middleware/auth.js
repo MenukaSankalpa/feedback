39e6e0b9b34693e20f1892e3af30bcf154444570
H.Menuka Sankalpa <menuka.sankalpa@gmail.com>
2026-02-04 17:17:51 +0530

    update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,10 +2,10 @@ import jwt from "jsonwebtoken";
 
 export default function auth(req, res, next) {
   const token = req.headers.authorization?.split(" ")[1];
-  if (!token) return res.sendStatus(401);
+  if (!token) return res.status(401).json({ message: "Unauthorized" });
 
   jwt.verify(token, "secretkey", (err, user) => {
-    if (err) return res.sendStatus(403);
+    if (err) return res.status(403).json({ message: "Invalid token" });
     req.user = user;
     next();
   });
