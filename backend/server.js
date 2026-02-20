692389f41af29819ad96f24ba3259dc32d2f6bd5
H.Menuka Sankalpa <menuka.sankalpa@gmail.com>
2026-02-20 08:39:10 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import feedbackRoutes from "./routes/feedback.js";
 import adminRoutes from "./routes/admin.js";
 
+dotenv.config();
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/feedback", feedbackRoutes); // user feedback
-app.use("/api/admin", adminRoutes);       // admin/user login + fetch feedback
+app.use("/api/feedback", feedbackRoutes);
+app.use("/api/admin", adminRoutes);
 
-app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
